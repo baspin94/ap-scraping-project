75a4fb295f3c5f52c138d876ade193b99b3dd399
Bianca Aspin <bianca@mj94.net>
2023-11-01 12:45:46 -0700

    Initial test code for extracting links from page.

--- scraper.py
+++ scraper.py
@@ -1,1 +1,14 @@
 from bs4 import BeautifulSoup
+import requests
+
+site = requests.get("https://www.nps.gov/yose/planyourvisit/accessibility.htm").text
+
+doc = BeautifulSoup(site, 'html.parser')
+
+main_content = doc.select(".ColumnMain")[0]
+
+links = main_content.select("a")
+
+for link in links:
+    print(link.text)
+    print(link.attrs['href'])
